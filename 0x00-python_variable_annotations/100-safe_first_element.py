2d4a59a646705744c09776e7a066477dcfd95c85
mcrymbo <croco2023@gmail.com>
2024-03-14 20:04:38 +0300

    type checking

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Duck-typed annotations
+The types of the elements of the input are not known
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
