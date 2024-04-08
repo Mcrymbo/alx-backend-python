de3f82288866bd4afdb1c0abc532fe797479f619
mcrymbo <croco2023@gmail.com>
2024-04-08 16:44:14 +0300

    integration test: fixture

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,8 @@ test client.GithubOrgClient
 import unittest
 from unittest.mock import Mock, patch, PropertyMock
 from client import GithubOrgClient
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -35,3 +36,58 @@ class TestGithubOrgClient(unittest.TestCase):
             test_class = GithubOrgClient('test')
             result = test_class._public_repos_url
             self.assertEqual(result, payload["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """ test a list of repos """
+        payload = [{'name': 'Google'}, {"name": "Twitter"}]
+        mock_json.return_value = payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock:
+            mock.return_value = "hello/world"
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+            check = [data["name"] for data in payload]
+            self.assertEqual(result, check)
+
+            mock.assert_called_once()
+            mock_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        ''' test has_licenced method '''
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
+
+@parameterized_class(
+         ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+         TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    Implements integration test for fixtures
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        class method called before the class is run
+        """
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+        cls.mock = cls.get_patcher.start()
+
+    @classmethod
+    def teardownClass(cls):
+        ''' called after all tests in a class have run '''
+        cls.get_patcher.stop("google")
